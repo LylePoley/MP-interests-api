6cb2f580b3f5ca5c42473c640fb5da2f31f95658
Lyle Poley <poleylyle@gmail.com>
2025-08-04 20:29:41 +0100

    Removed unecessary files

--- backend/client/client.py
+++
@@ -1,3 +0,0 @@
-from httpx import Client
-
-members_client = Client(base_url="https://members.parliament.uk/", follow_redirects=True)
--- backend/db.py
+++
@@ -1,7 +0,0 @@
-# db.py
-from sqlmodel import SQLModel, create_engine, Session
-
-engine = create_engine("sqlite:///members.db")  # or Postgres URI
-
-def get_session():
-    return Session(engine)
