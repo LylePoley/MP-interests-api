0caf2fc85b14b435e1ba1c3923570eb5b5f86a7b
Lyle Poley <poleylyle@gmail.com>
2025-08-05 15:31:53 +0100

    Removed unised settings

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,7 +5,7 @@ class LogLevel(Enum):
     NOTSET = logging.NOTSET
     DEBUG = logging.DEBUG
     INFO = logging.INFO
-    WARNING = logging.WARNING
+    WARNING= logging.WARNING
     CRITICAL = logging.CRITICAL
     ERROR = logging.ERROR
 
@@ -14,8 +14,6 @@ class Settings:
     FRONTEND_HOST: str = "http://localhost:8000"
     SQLITE_DB: str = "sqlite:///members.db"
 
-    GETATTR_LOG_LEVEL: LogLevel = LogLevel.CRITICAL
-    UPSERT_LOG_LEVEL: LogLevel = LogLevel.WARNING
     LOG_LEVEL: LogLevel = LogLevel.INFO
 
 
