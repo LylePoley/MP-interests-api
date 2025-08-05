1f0def102007d7e490fe9ac33936f9fbeb7cbff7
Lyle Poley <poleylyle@gmail.com>
2025-08-05 15:36:02 +0100

    changed member_search  to use filters

--- backend/routes/members.py
+++ backend/routes/members.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Depends
+from sqlmodel import Session, select, col, func
 from typing import List
 
 from backend.models import Member
-from backend.core import engine
-from backend.core.queries import search_members as db_search_members
+from backend.core.db import get_session
+import backend.core.filters as filter
 from datetime import datetime
 
 router = APIRouter(prefix="/members", tags=["members"])
 
 @router.get("/search", response_model=List[Member])
-def search_members(
+def search_members(*, session: Session = Depends(get_session),
     name: str | None = Query(None),
     party: str | None = Query(None),
     house: int | None = Query(None),
@@ -19,13 +20,15 @@ def search_members(
     take: int = Query(20),
 ) -> List[Member]:
     """Get members with multiple optional filters."""
-    return db_search_members(
-        engine,
-        name=name,
-        party=party,
-        house=house,
-        membership_started_since=membership_started_since,
-        membership_ended_since=membership_ended_since,
-        skip=skip,
-        take=take
-    )
+    statement = select(Member)
+    statement = filter.by_member_name(statement, name)
+    statement = filter.by_party(statement, party)
+    statement = filter.by_house(statement, house)
+    statement = filter.by_membership_start_date(statement, membership_started_since)
+    statement = filter.by_membership_end_date(statement, membership_ended_since)
+
+    statement = statement.offset(skip).limit(take)
+
+    return list(session.exec(statement).all())
+
+
