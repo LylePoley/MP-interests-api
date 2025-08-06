40017c66518e8522fbfba0d2303b436325afd4fc
Lyle Poley <poleylyle@gmail.com>
2025-08-06 13:07:38 +0100

    Added typehints to database filters

--- backend/core/filters.py
+++ backend/core/filters.py
@@ -1,41 +1,63 @@
-from sqlmodel import func, col
-
-from datetime import datetime
 from backend.models import Member, Party, Interest
-from datetime import date
 
-def by_member_name(statement, name: str | None):
+from sqlmodel import func, col
+from typing import Optional, Any
+from sqlmodel.sql.expression import Select, SelectOfScalar
+from datetime import datetime
+
+
+def by_member_name(
+    statement: Select[Any] | SelectOfScalar[Any],
+    name: Optional[str]
+) -> Select[Any] | SelectOfScalar[Any]:
     if name:
         return statement.where(func.lower(Member.name_display_as).like(f"%{name.lower()}%"))
     return statement
 
-def by_party(statement, party: str | None):
+def by_party(
+    statement: Select[Any] | SelectOfScalar[Any],
+    party: Optional[str]
+) -> Select[Any] | SelectOfScalar[Any]:
     if party:
         return statement.where(func.lower(Party.name).like(f"%{party.lower()}%"))
     return statement
 
-def by_house(statement, house: int | None):
+def by_house(
+    statement: Select[Any] | SelectOfScalar[Any],
+    house: Optional[int]
+) -> Select[Any] | SelectOfScalar[Any]:
     if house:
         return statement.where(Member.house == house)
     return statement
 
-def by_membership_start_date(statement, membership_started_since: datetime | None):
+def by_membership_start(
+    statement: Select[Any] | SelectOfScalar[Any],
+    membership_started_since: Optional[datetime]
+) -> Select[Any] | SelectOfScalar[Any]:
     if membership_started_since:
         return statement.where(col(Member.membership_start_date) >= membership_started_since)
     return statement
 
-def by_membership_end_date(statement, membership_ended_since: datetime | None):
+def by_membership_end(
+    statement: Select[Any] | SelectOfScalar[Any],
+    membership_ended_since: Optional[datetime]
+) -> Select[Any] | SelectOfScalar[Any]:
     if membership_ended_since:
         return statement.where(col(Member.membership_end_date) <= membership_ended_since)
     return statement
 
-def by_interest_published_date_after(statement, published_after: str | None):
+def by_interest_published_after(
+    statement: Select[Any] | SelectOfScalar[Any],
+    published_after: Optional[datetime]
+) -> Select[Any] | SelectOfScalar[Any]:
     if published_after:
-        return statement.where(col(Interest.published_date) >= date.fromisoformat(published_after))
+        return statement.where(col(Interest.published_date) >= published_after)
     return statement
 
-def by_interest_published_date_before(statement, published_before: str | None):
+def by_interest_published_before(
+    statement: Select[Any] | SelectOfScalar[Any],
+    published_before: Optional[datetime]
+) -> Select[Any] | SelectOfScalar[Any]:
     if published_before:
-        return statement.where(col(Interest.published_date) <= date.fromisoformat(published_before))
+        return statement.where(col(Interest.published_date) <= published_before)
     return statement
-
