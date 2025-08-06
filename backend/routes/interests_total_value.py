39bdb4f786fe142fcfb475def6522eed76455a4f
Lyle Poley <poleylyle@gmail.com>
2025-08-06 15:32:33 +0100

    Added documentation to api endpoints.

--- backend/routes/interests_total_value.py
+++ backend/routes/interests_total_value.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Query, Depends
 from sqlmodel import Session, select, col, func
-from typing import List
+from typing import List, Annotated
 
 from backend.models import Interest, Member, MonetaryValueField
 from backend.core.db import get_session
@@ -19,22 +19,69 @@ class MemberWithTotalInterestValue(BaseModel):
 def search_members_with_grouped_interest_values(
     *,
     session: Session = Depends(get_session),
-    member_name: str | None = Query(None),
-    party: str | None = Query(None),
-    house: int | None = Query(None),
-    published_before: datetime | None = Query(None),
-    published_after: datetime | None = Query(None),
-    skip: int = Query(0),
-    take: int = Query(20),
+    member_name: Annotated[str | None, Query(description="Name of the member to search for. Supports partial matches and is case insensitive e.g. 'John' and 'sm' will match 'John Smith'")] = None,
+    party: Annotated[str | None, Query(description="Name of the party to filter members by, supports partial matches and is case insensitive.")] = None,
+    house: Annotated[int | None, Query(description="House ID to filter members by. 1 for Commons, 2 for Lords")] = None,
+    published_before: Annotated[datetime | None, Query(description="Filter by interests published before this date in ISO format (YYYY-MM-DD).")] = None,
+    published_after: Annotated[datetime | None, Query(description="Filter by interests published after this date in ISO format (YYYY-MM-DD).")] = None,
+    skip: Annotated[int, Query(description="Number of records to skip for pagination. E.g. 50 will skip the first 50 records")] = 0,
+    take: Annotated[int | None, Query(description="Number of records to return. E.g. 20 will return the next 20 records after the skipped ones")] = None,
 ) -> List[MemberWithTotalInterestValue]:
     """
-    Get members with optional filters and total of their interest values.
+    Search members and when their interests where published. Results are returned sorted in descending value of interests.
+
+    Arguments:
+
+    - member_name: Name of the member to search for. Supports partial matches and is case insensitive e.g. 'John' and 'sm' will match 'John Smith'
+    - party: Name of the party to filter members by. Supports partial matches and is case insensitive.
+    - house: House ID to filter members by. 1 for Commons, 2 for Lords.
+    - published_before: Filter by interests published before this date. Supports ISO format (YYYY-MM-DD).
+    - published_after: Filter by interests published after this date. Supports ISO format (YYYY-MM-DD).
+    - skip: Number of records to skip for pagination. E.g. 50 will skip the first 50 records.
+    - take: Number of records to return. E.g. 20 will return the next 20 records after the skipped ones. If absent, all matching records will be returned.
+
+    Returns:
+
+    members: List[MemberWithTotalInterestValue]
+
+    A list elements of type `MemberWithTotalInterestValue`, which contains a .member and a .total_interests_value field.
+    Returns all members matching the search criteria, along with the total value of their interests.
+
+    Examples:
+    Get the total value of interests published in 2024 or after for members whose name contains 'diane':
+    - `HOST/interests/search?name=diane&take=1&published_after=2024`
+    ```json
+    [
+        {
+            "member": {
+                "name_display_as": "Ms Diane Abbott",
+                "name_list_as": "Abbott, Ms Diane",
+                "name_address_as": "Ms Abbott",
+                "thumbnail_url": "https://members-api.parliament.uk/api/Members/172/Thumbnail",
+                "house": 1,
+                "membership_from_id": 4074,
+                "membership_end_date": null,
+                "status_is_active": true,
+                "id": 172,
+                "name_full_title": "Rt Hon Diane Abbott MP",
+                "gender": "F",
+                "party_id": 8,
+                "membership_from": "Hackney North and Stoke Newington",
+                "membership_start_date": "1987-06-11T00:00:00",
+                "membership_end_reason": null,
+                "status_start_date": "2024-07-04T00:00:00"
+            },
+            "total_interests_value": 29090.0
+        }
+    ]
+    ```
     """
 
     statement = (
         select(Member, func.coalesce(func.sum(MonetaryValueField.value), 0).label("total"))
         .join(Interest, col(Interest.member_id) == col(Member.id), isouter=True)
         .join(MonetaryValueField, col(MonetaryValueField.interest_id) == col(Interest.id), isouter=True)
+        .order_by(col("total").desc())
     )
 
     statement = filter.by_member_name(statement, name=member_name)
@@ -43,7 +90,11 @@ def search_members_with_grouped_interest_values(
     statement = filter.by_interest_published_after(statement, published_after)
     statement = filter.by_interest_published_before(statement, published_before)
 
-    statement = statement.offset(skip).limit(take)
+    statement = statement.offset(skip)
+
+    if take:
+        statement = statement.limit(take)
+
     statement = statement.group_by(col(Member.id))
 
     results = session.exec(statement).all()
@@ -51,5 +102,3 @@ def search_members_with_grouped_interest_values(
     return [
         MemberWithTotalInterestValue(member=row[0], total_interests_value=row[1] if row[1] else 0.0) for row in results
     ]
-
-
--- backend/routes/members.py
+++ backend/routes/members.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Query, Depends
 from sqlmodel import Session, select
-from typing import List
+from typing import List, Annotated
 
 from backend.models import Member
 from backend.core.db import get_session
@@ -11,15 +11,86 @@ router = APIRouter(prefix="/members", tags=["members"])
 
 @router.get("/search", response_model=List[Member], operation_id="search_members")
 def search_members(*, session: Session = Depends(get_session),
-    name: str | None = Query(None),
-    party: str | None = Query(None),
-    house: int | None = Query(None),
-    membership_started_since: datetime | None = Query(None),
-    membership_ended_since: datetime | None = Query(None),
-    skip: int = Query(0),
-    take: int = Query(20),
+    name: Annotated[str | None,
+                    Query(description="Name of the member to search for. Supports partial matches and is case insensitive.",
+                          example="kier")]=None,
+    party: Annotated[str | None,
+                    Query(description="Name of the party to filter members by, supports partial matches and is case insensitive.",
+                          example="lab")]=None,
+    house: Annotated[int | None,
+                    Query(description="House ID members by. 1 for Commons, 2 for Lords")]=None,
+    membership_started_since: Annotated[datetime | None,
+                    Query(description="Filter by members whose membership started since this date in ISO format (YYYY-MM-DD).",
+                          example="2012-12-25")]=None,
+    membership_ended_since: Annotated[datetime | None,
+                    Query(description="Filter by members whose membership ended since this date in ISO format (YYYY-MM-DD).")]=None,
+    skip: Annotated[int,
+                    Query(description="Number of records to skip for pagination. E.g. 50 will skip the first 50 records")]=0,
+    take: Annotated[int | None,
+                    Query(description="Number of records to return. E.g. 20 will return the next 20 records after the skipped ones")]=None,
 ) -> List[Member]:
-    """Get members with multiple optional filters."""
+    """
+    Search members matching the given criteria.
+
+    Arguments:
+    - name: Name of the member to search for. Supports partial matches and is case insensitive e.g. 'John' and 'sm' will match 'John Smith'
+    - party: Name of the party to filter members by. Supports partial matches and is case insensitive.
+    - house: House ID to filter members by. 1 for Commons, 2 for Lords.
+    - membership_started_since: Filter by members whose membership started since this date. Supports ISO format (YYYY-MM-DD).
+    - membership_ended_since: Filter by members whose membership ended since this date. Supports ISO format (YYYY-MM-DD).
+    - skip: Number of records to skip for pagination. E.g. 50 will skip the first 50 records.
+    - take: Number of records to return. E.g. 20 will return the next 20 records after the skipped ones.
+
+    Returns:
+
+    members: List[Member]
+
+    List of the details of each member matching the criteria.
+
+    Examples:
+    Get first two members in the database who have started since 2025 from the Labour party:
+    - `HOST/members/search?party=lab&embership_started_since=2025&take=2`
+    ```json
+    [
+        {
+            "name_list_as":"May of Maidenhead, B.",
+            "name_display_as":"Baroness May of Maidenhead",
+            "name_address_as":null,
+            "thumbnail_url":"https://members-api.parliament.uk/api/Members/8/Thumbnail",
+            "house":2,
+            "membership_from_id":4,
+            "membership_end_date":null,
+            "status_is_active":true,
+            "id":8,
+            "name_full_title":"The Rt Hon. the Baroness May of Maidenhead",
+            "gender":"F",
+            "party_id":4,
+            "membership_from":"Life peer",
+            "membership_start_date":"2024-08-21T00:00:00",
+            "membership_end_reason":null,
+            "status_start_date":"2024-08-21T00:00:00"
+        },
+        {
+            "name_list_as":"Pickles, L.",
+            "name_display_as":"Lord Pickles",
+            "name_address_as":null,
+            "thumbnail_url":"https://members-api.parliament.uk/api/Members/33/Thumbnail",
+            "house":2,
+            "membership_from_id":4,
+            "membership_end_date":null,
+            "status_is_active":true,
+            "id":33,
+            "name_full_title":"The Rt Hon. the Lord Pickles",
+            "gender":"M",
+            "party_id":4,
+            "membership_from":"Life peer",
+            "membership_start_date":"2018-06-18T00:00:00",
+            "membership_end_reason":null,
+            "status_start_date":"2018-06-18T00:00:00"
+        }
+    ]
+    ```
+    """
     statement = select(Member)
     statement = filter.by_member_name(statement, name)
     statement = filter.by_party(statement, party)
