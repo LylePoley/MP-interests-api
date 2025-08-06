256b2dd5c7067060e2fddf996a9de91b41d5eaa4
Lyle Poley <poleylyle@gmail.com>
2025-08-07 00:34:50 +0100

    Minor edits

--- backend/api_server.py
+++ backend/api_server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Response
 from fastapi_mcp import FastApiMCP
 
-
 from backend.routes.members import router as members_router
 from backend.routes.interests_total_value import router as interests_total_value_router
 
--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,2 +1,2 @@
 from backend.models.sqlmodel_models import Member, Party, Interest, InterestCategory, InterestField, MonetaryValueField
-from backend.models.parsers import member_from_dict, interest_from_dict
+from backend.models.parsers import member_and_party_from_dict, interest_from_dict
