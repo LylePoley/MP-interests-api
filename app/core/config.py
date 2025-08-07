112b690e86f48e60b4bad97cb665ce503169aaba
Lyle Poley <poleylyle@gmail.com>
2025-08-08 00:52:39 +0100

    changed database location to use absolute path rather than relative path

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 import logging
 from enum import Enum
-
+import os
 
 class LogLevel(Enum):
     NOTSET = logging.NOTSET
@@ -14,7 +14,9 @@ class LogLevel(Enum):
 class Settings:
     FASTAPI_HOST: str = "localhost"  # or "127.0.0.1"
     FASTAPI_PORT: int = 8000
-    SQLITE_DB: str = "sqlite:///./app/data/members.db"
+    CURRENT_WORKING_DIR: str = os.getcwd()
+    SQLITE_DB_PATH: str = os.path.join(CURRENT_WORKING_DIR, "app", "data", "members.db")
+    SQLITE_DB: str = "sqlite:///" + SQLITE_DB_PATH
 
     LOG_LEVEL: LogLevel = LogLevel.INFO
 
